ff39b6cddeabfab5c4a83fed808e2c6957cde4f6
Dung CTS <102821755+dung-1@users.noreply.github.com>
2024-10-07 16:33:47 +0700

    feat:add table cate and product in database

--- Api-Service/Program.cs
+++ Api-Service/Program.cs
@@ -1,11 +1,31 @@
+﻿using Api_Service.Data;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;  
+using Microsoft.Extensions.DependencyInjection;
+using Api_Service.Common;
+using Api_Service.Mappings;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// Thêm dịch vụ DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Thêm dịch vụ Global
+GlobalHelper.RegisterServices(builder.Services);
+GlobalHelper.AddMappingProfiles(builder.Services);
+
+// Thêm dịch vụ AddAutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
 
 var app = builder.Build();
 
